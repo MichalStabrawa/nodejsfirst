037c846fa5735d147815e5036d3fd6ed507a7843
Michal Stabrawa <mistaaqua@gmail.com>
2019-06-02 13:23:55 +0200

    commit

--- app.js
+++ app.js
@@ -6,8 +6,13 @@
 // res.send(test)
 // })
 // app.listen(3000)
-
 const fs = require('fs')
 const fileName = 'target.txt'
 
-fs.watch(fileName, () => console.log(`File changed!`))
+fs.readFile(fileName, (err, data) => {
+  if (err) {
+    console.log(err)
+  }
+  console.log(data.toString())
+})
+console.log('Node is assync programming')
