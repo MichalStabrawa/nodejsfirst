d4beb527c2a5e38864368a892cac40afff65ea46
Michal Stabrawa <mistaaqua@gmail.com>
2019-06-02 11:25:22 +0200

    create server

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const { sum, min } = require('./hellpers')
 const test = require('./test')
 
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  res.end('hello world from node js')
+})
+
+server.listen(3000)
+
 // console.log('PROCESS', process)
 const total = sum(10, 200)
 const totalmin = min(200, 10)
