121bbd3f4a344705f0a568a33392dc1cd283ccde
Michal Stabrawa <mistaaqua@gmail.com>
2019-06-02 11:21:09 +0200

    add destructor object

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const hellpers = require('./hellpers')
+const { sum, min } = require('./hellpers')
 const test = require('./test')
 
 // console.log('PROCESS', process)
-const total = hellpers.sum(10, 200)
+const total = sum(10, 200)
+const totalmin = min(200, 10)
 const totaltest = test.test(10, 200)
 
 console.log('Total' + total)
-console.log('Test node module' + '' + totaltest)
+console.log('Min test: ' + totalmin)
+console.log('Test node module ' + totaltest)
