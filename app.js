c21b3b9c625f9d6a2131b3a73ed3744f92510ae1
Michal Stabrawa <mistaaqua@gmail.com>
2019-06-02 11:56:54 +0200

    first express server

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
-const { sum, min } = require('./hellpers')
-const test = require('./test')
+const express = require('express')
 
-const http = require('http')
+const app = express()
 
-const server = http.createServer((req, res) => {
-  res.end('hello world from node js nodemon')
+app.get('/', (req, res) => {
+  res.send('hey express')
 })
-
-server.listen(3000)
-
-// console.log('PROCESS', process)
-const total = sum(10, 200)
-const totalmin = min(200, 10)
-const totaltest = test.test(10, 200)
-
-console.log('Total' + total)
-console.log('Min test: ' + totalmin)
-console.log('Test node module ' + totaltest)
+app.listen(3000)
